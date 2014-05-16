6e041781addc53d6359a7a054460cca94fc912fc
boyang070822 <boyang070822@gmail.com>
2014-05-16 11:34:45 +0800

    change updateDict.sh

--- bin/updateDict.sh
+++ bin/updateDict.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 baseDir=`dirname $0`/..
 JAR=$baseDir/target/lldaMahout-1.0-SNAPSHOT-jar-with-dependencies.jar
-MAIN=com.elex.bigdata.llda.mahout.data.dictionary.UpdateDictDriver
+MAIN=com.elex.bigdata.llda.mahout.dictionary.UpdateDictDriver
 rootPath=/user/hadoop/user_category/lldaMahout
 echo "hadoop jar $JAR $MAIN --input $1 --dictionary ${rootPath}/dictionary"
 hadoop jar $JAR $MAIN --input $1 --dictionary ${rootPath}/dictionary
