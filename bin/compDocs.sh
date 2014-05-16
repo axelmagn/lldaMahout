4d4412df7a9415a15656000477118a2466273c89
boyang070822 <boyang070822@gmail.com>
2014-05-16 17:43:56 +0800

    change ComplementLDocDriver

--- bin/compDocs.sh
+++ bin/compDocs.sh
@@ -3,4 +3,4 @@ baseDir=`dirname $0`/..
 JAR=$baseDir/target/lldaMahout-1.0-SNAPSHOT-jar-with-dependencies.jar
 MAIN=com.elex.bigdata.llda.mahout.data.complementdocs.ComplementLDocDriver
 rootPath=/user/hadoop/user_category/lldaMahout
-hadoop jar $JAR $MAIN --input $1 --leftInput $2 --docsRoot ${rootPath}/docs
+hadoop jar $JAR $MAIN --input $1 --leftInput $2 --docsRoot ${rootPath}/docs --dictionary ${rootPath}/dictionary
