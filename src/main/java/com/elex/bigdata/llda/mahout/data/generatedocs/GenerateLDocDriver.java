be4ee66f97a3ec04f11f12b7931dc208ae239aba
boyang070822 <boyang070822@gmail.com>
2014-05-16 15:55:19 +0800

    change generateLDocDriver

--- src/main/java/com/elex/bigdata/llda/mahout/data/generatedocs/GenerateLDocDriver.java
+++ src/main/java/com/elex/bigdata/llda/mahout/data/generatedocs/GenerateLDocDriver.java
@@ -4,11 +4,10 @@ import com.elex.bigdata.llda.mahout.data.LabeledDocumentWritable;
 import com.elex.bigdata.llda.mahout.data.inputformat.CombineTextInputFormat;
 import com.elex.bigdata.llda.mahout.dictionary.UpdateDictDriver;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.mahout.common.AbstractJob;
@@ -47,6 +46,9 @@ public class GenerateLDocDriver extends AbstractJob {
     String resourcesDir=getOption(RESOURCE_OPTION_NAME);
     String uidPath=docsRoot+File.separator+"uid";
     Configuration conf=new Configuration();
+    FileSystem fs= FileSystem.get(conf);
+    if(fs.exists(new Path(docsPath)))
+      fs.delete(new Path(docsPath));
     Job genLDocJob=prepareJob(conf,inputPath,new Path(docsPath),dictRoot,resourcesDir,uidPath);
     genLDocJob.submit();
     genLDocJob.waitForCompletion(true);
