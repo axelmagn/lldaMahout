4cc4d81f7e4b9d6894eff33edbaad070ef99f30d
boyang070822 <boyang070822@gmail.com>
2014-05-19 14:51:32 +0800

    create estDocs.sh

--- bin/estDocs.sh
+++ bin/estDocs.sh
@@ -3,7 +3,7 @@ baseDir=`dirname $0`/..
 JAR=$baseDir/target/lldaMahout-1.0-SNAPSHOT-jar-with-dependencies.jar
 MAIN=com.elex.bigdata.llda.mahout.mapreduce.LLDADriver
 rootPath=/user/hadoop/user_category/lldaMahout
-echo "hadoop jar $JAR $MAIN  --input ${rootPath}/docs/$1 -k 20 --output ${rootPath}/models --maxIter 100 --dictionary ${rootPath}/dictionary/dict \
-      -dt ${rootPath}/docTopics --num_reduce_tasks 1"
+echo "hadoop jar $JAR $MAIN  --input ${rootPath}/docs/$1 -k 20 --output ${rootPath}/models --maxIter 50 -mipd 500 --dictionary ${rootPath}/dictionary/dict \
+      -dt ${rootPath}/docTopics -mt ${rootPath}/tmpModels --num_reduce_tasks 1"
 hadoop jar $JAR $MAIN  --input ${rootPath}/docs/$1 -k 20 --output ${rootPath}/models --maxIter 50 -mipd 500 --dictionary ${rootPath}/dictionary/dict \
 -dt ${rootPath}/docTopics -mt ${rootPath}/tmpModels --num_reduce_tasks 1
