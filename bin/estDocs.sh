4ce6ba4a91c0b474780b9cad2f977938027d7987
boyang070822 <boyang070822@gmail.com>
2014-05-20 16:42:58 +0800

    fix a bug in LLDA(Inference)Mapper

--- bin/estDocs.sh
+++ bin/estDocs.sh
@@ -5,5 +5,5 @@ MAIN=com.elex.bigdata.llda.mahout.mapreduce.LLDADriver
 rootPath=/user/hadoop/user_category/lldaMahout
 echo "hadoop jar $JAR $MAIN  --input ${rootPath}/docs/$1 -k 20 --output ${rootPath}/models --maxIter 4 -mipd 50 --dictionary ${rootPath}/dictionary/dict \
       -dt ${rootPath}/docTopics -mt ${rootPath}/tmpModels --num_reduce_tasks 1"
-hadoop jar $JAR $MAIN  --input ${rootPath}/docs/$1 -k 21 --output ${rootPath}/models --maxIter 4 -mipd 50 --dictionary ${rootPath}/dictionary/dict \
+hadoop jar $JAR $MAIN  --input ${rootPath}/docs/$1 -k 5 --output ${rootPath}/models --maxIter 4 -mipd 50 --dictionary ${rootPath}/dictionary/dict \
 -dt ${rootPath}/docTopics -mt ${rootPath}/tmpModels --num_reduce_tasks 1
