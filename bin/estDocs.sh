45db6ce57900f0c23aaa07bea5cfde8ff1ae08ec
boyang070822 <boyang070822@gmail.com>
2014-05-20 17:09:09 +0800

    fix a bug in LLDA(Inference)Mapper

--- bin/estDocs.sh
+++ bin/estDocs.sh
@@ -3,7 +3,7 @@ baseDir=`dirname $0`/..
 JAR=$baseDir/target/lldaMahout-1.0-SNAPSHOT-jar-with-dependencies.jar
 MAIN=com.elex.bigdata.llda.mahout.mapreduce.LLDADriver
 rootPath=/user/hadoop/user_category/lldaMahout
-echo "hadoop jar $JAR $MAIN  --input ${rootPath}/docs/$1 -k 6 --output ${rootPath}/models --maxIter 2 -mipd 50 --dictionary ${rootPath}/dictionary/dict \
+echo "hadoop jar $JAR $MAIN  --input ${rootPath}/docs/$1 -k 6 --output ${rootPath}/models --maxIter 20 -mipd 5 --dictionary ${rootPath}/dictionary/dict \
       -dt ${rootPath}/docTopics -mt ${rootPath}/tmpModels --num_reduce_tasks 1"
-hadoop jar $JAR $MAIN  --input ${rootPath}/docs/$1 -k 6 --output ${rootPath}/models --maxIter 2 -mipd 50 --dictionary ${rootPath}/dictionary/dict \
+hadoop jar $JAR $MAIN  --input ${rootPath}/docs/$1 -k 6 --output ${rootPath}/models --maxIter 20 -mipd 5 --dictionary ${rootPath}/dictionary/dict \
 -dt ${rootPath}/docTopics -mt ${rootPath}/tmpModels --num_reduce_tasks 1
