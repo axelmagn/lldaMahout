1846d762c727e7b3ec970506ff9a183968c6669e
boyang070822 <boyang070822@gmail.com>
2014-05-29 17:51:49 +0800

    change fiveMinInf.sh

--- bin/fiveMinInf.sh
+++ bin/fiveMinInf.sh
@@ -4,8 +4,10 @@ startTime=`date +%Y%m%d%H%M%S -d "-10 mins"`
 endTime=`date +%Y%m%d%H%M%S -d "-5 mins"`
 preDay=`date +%Y%m%d -d "-1 days"`
 day=`date +%Y%m%d `
-sh $baseDir/../Url_Count/bin/AccumulateUrlCount.sh $startTime $endTime
-sh $baseDir/bin/updateDict.sh url_count/*/${startTime}_${endTime}
-sh $baseDir/bin/getInfDocs.sh url_count/*/${day}* to${preDay} ${day}
-sh $baseDir/bin/infDocs.sh ${day}
-sh $baseDir/bin/etl.sh inf result
+logFile=/data/log/user_category/processLog/llda/hadoopLLDA.log
+echo "fiveMinInf $startTime" >> $logFile
+sh $baseDir/../Url_Count/bin/AccumulateUrlCount.sh $startTime $endTime >> $logFile
+sh $baseDir/bin/updateDict.sh url_count/*/${startTime}_${endTime} >> $logFile
+sh $baseDir/bin/getInfDocs.sh url_count/*/${day}* to${preDay} ${day} >> $logFile
+sh $baseDir/bin/infDocs.sh ${day}  >> $logFile
+sh $baseDir/bin/etl.sh inf result  >> $logFile
