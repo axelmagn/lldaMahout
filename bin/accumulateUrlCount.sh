d06e53f962f823d27ec0e8de0184f39b16a3e0b5
boyang070822 <boyang070822@gmail.com>
2014-06-09 11:52:55 +0800

    accumulate url count

--- bin/accumulateUrlCount.sh
+++ bin/accumulateUrlCount.sh
@@ -3,4 +3,5 @@ baseDir=`dirname $0`/..
 JAR=$baseDir/target/lldaMahout-1.0-SNAPSHOT-jar-with-dependencies.jar
 MAIN=com.elex.bigdata.llda.mahout.data.accumulateurlcount.Accumulate
 outputBase=url_count/all_projects
+echo "java -cp $JAR $MAIN --outputBase $outputBase --startTime $1 --endTime $2"
 java -cp $JAR $MAIN --outputBase $outputBase --startTime $1 --endTime $2
