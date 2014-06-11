bef4bb74646ac2809ce5d49327019d66f67912d7
boyang070822 <boyang070822@gmail.com>
2014-06-11 10:18:13 +0800

    do not use lock in PutUrlCount

--- src/main/java/com/elex/bigdata/llda/mahout/data/accumulateurlcount/PutUrlCount.java
+++ src/main/java/com/elex/bigdata/llda/mahout/data/accumulateurlcount/PutUrlCount.java
@@ -20,55 +20,54 @@ import java.util.concurrent.ConcurrentHashMap;
  * To change this template use File | Settings | File Templates.
  */
 public class PutUrlCount {
-  private Map<Path,BufferedWriter> writers=new ConcurrentHashMap<Path, BufferedWriter>();
+  private Map<Path, BufferedWriter> writers = new ConcurrentHashMap<Path, BufferedWriter>();
   private FileSystem fs;
-  public PutUrlCount(FileSystem fs){
-    this.fs=fs;
+
+  public PutUrlCount(FileSystem fs) {
+    this.fs = fs;
   }
 
-  public PutUrlCountRunnable getRunnable(Path filePath,Map<String,Map<String,Integer>> uidUrlCountMap) throws IOException {
-     BufferedWriter writer;
-     if(writers.containsKey(filePath))
-       writer=writers.get(filePath);
-     else {
-       FSDataOutputStream outputStream=fs.create(filePath);
-       writer=new BufferedWriter(new OutputStreamWriter(outputStream));
-       writers.put(filePath,writer);
-     }
-    System.out.println(filePath.getName()+" uid size "+uidUrlCountMap.size());
-     return new PutUrlCountRunnable(writer,uidUrlCountMap);
+  public PutUrlCountRunnable getRunnable(Path filePath, Map<String, Map<String, Integer>> uidUrlCountMap) throws IOException {
+    BufferedWriter writer;
+    if (writers.containsKey(filePath))
+      writer = writers.get(filePath);
+    else {
+      FSDataOutputStream outputStream = fs.create(filePath);
+      writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+      writers.put(filePath, writer);
+    }
+    System.out.println(filePath.getName() + " uid size " + uidUrlCountMap.size());
+    return new PutUrlCountRunnable(writer, uidUrlCountMap);
 
   }
 
-public static class PutUrlCountRunnable implements Runnable {
-  Writer writer;
-  private Map<String,Map<String,Integer>> uidUrlCountMap=new HashMap<String, Map<String, Integer>>();
-  public PutUrlCountRunnable(Writer writer, Map<String, Map<String, Integer>> uidUrlCountMap) {
-    this.uidUrlCountMap=uidUrlCountMap;
-    this.writer=writer;
-  }
+  public static class PutUrlCountRunnable implements Runnable {
+    Writer writer;
+    private Map<String, Map<String, Integer>> uidUrlCountMap = new HashMap<String, Map<String, Integer>>();
 
-  @Override
-  public void run() {
-    try {
-      for(Map.Entry<String,Map<String,Integer>> entry: uidUrlCountMap.entrySet()){
-        String uid=entry.getKey();
-        Map<String,Integer> urlCount=entry.getValue();
+    public PutUrlCountRunnable(Writer writer, Map<String, Map<String, Integer>> uidUrlCountMap) {
+      this.uidUrlCountMap = uidUrlCountMap;
+      this.writer = writer;
+    }
 
-        for(Map.Entry<String,Integer> urlCountEntry: urlCount.entrySet()){
-          StringBuilder builder=new StringBuilder();
-          builder.append(uid+"\t");
-          builder.append(urlCountEntry.getKey()+"\t"+urlCountEntry.getValue());
-          synchronized (writer){
-            writer.write(builder.toString());
-            writer.write("\r\n");
+    @Override
+    public void run() {
+      try {
+        for (Map.Entry<String, Map<String, Integer>> entry : uidUrlCountMap.entrySet()) {
+          String uid = entry.getKey();
+          Map<String, Integer> urlCount = entry.getValue();
+
+          for (Map.Entry<String, Integer> urlCountEntry : urlCount.entrySet()) {
+            StringBuilder builder = new StringBuilder();
+            builder.append(uid + "\t");
+            builder.append(urlCountEntry.getKey() + "\t" + urlCountEntry.getValue());
+            writer.write(builder.toString() + "\r\n");
           }
         }
+        writer.flush();
+      } catch (IOException e) {
+        e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
       }
-      writer.flush();
-    } catch (IOException e) {
-      e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
     }
   }
-}
 }
