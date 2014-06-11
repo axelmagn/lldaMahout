b64a42c2cfae891bd101ae7efe594cd4eeca1eeb
boyang070822 <boyang070822@gmail.com>
2014-06-11 11:09:29 +0800

    revert

--- bin/accumulateUrlCount.sh
+++ bin/accumulateUrlCount.sh
@@ -3,5 +3,5 @@ baseDir=`dirname $0`/..
 JAR=$baseDir/target/lldaMahout-1.0-SNAPSHOT-jar-with-dependencies.jar
 MAIN=com.elex.bigdata.llda.mahout.data.accumulateurlcount.Accumulate
 outputBase=url_count/all_projects
-echo "hadoop jar $JAR -Xss256k $MAIN --outputBase $outputBase --startTime $1 --endTime $2"
-hadoop jar $JAR -Xss256k $MAIN --outputBase $outputBase --startTime $1 --endTime $2
+echo "hadoop jar $JAR  $MAIN --outputBase $outputBase --startTime $1 --endTime $2"
+hadoop jar $JAR  $MAIN --outputBase $outputBase --startTime $1 --endTime $2
