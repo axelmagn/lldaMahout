2a33986045fe6f509c37c2d21f291307effb4423
boyang070822 <boyang070822@gmail.com>
2014-06-18 15:26:33 +0800

    log inf UpdateDictMapper

--- src/main/java/com/elex/bigdata/llda/mahout/dictionary/UpdateDictMapper.java
+++ src/main/java/com/elex/bigdata/llda/mahout/dictionary/UpdateDictMapper.java
@@ -4,6 +4,8 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -15,8 +17,14 @@ import java.io.IOException;
  * To change this template use File | Settings | File Templates.
  */
 public class UpdateDictMapper extends Mapper<Object,Text,Text,NullWritable> {
+  private static final Logger log = LoggerFactory.getLogger(UpdateDictMapper.class);
   public void map(Object key,Text value ,Context context) throws IOException, InterruptedException {
      String[] uidUrlCount=value.toString().split("\t");
+     if(uidUrlCount.length<2)
+     {
+       log.warn("wrong uidUrlCount "+value.toString());
+       return;
+     }
      String url=uidUrlCount[1];
      context.write(new Text(url), NullWritable.get());
   }
