a278fb3938b6461a20642d5fc8e66dc634e9a49b
boyang070822 <boyang070822@gmail.com>
2014-06-27 18:23:08 +0800

    add hourInf.sh

--- bin/hourInf.sh
+++ bin/hourInf.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 baseDir=`dirname $0`/..
-startTime=`date +%Y%m%d%H%M%S -d "-10 mins"`
-endTime=`date +%Y%m%d%H%M%S -d "-5 mins"`
+startTime=`date +%Y%m%d%H%M%S -d "-2 hours"`
+endTime=`date +%Y%m%d%H%M%S -d "-1 hours"`
 preDay=`date +%Y%m%d -d "-1 days"`
 day=`date +%Y%m%d `
 logFile=/data/log/user_category/processLog/llda/hadoopLLDA.log
 echo "fiveMinInf $startTime" >> $logFile
 textInputRoot=url_count/all_projects
-sh $baseDir/../Url_Count/bin/AccumulateUrlCount.sh $startTime $endTime >> $logFile
+#sh $baseDir/../Url_Count/bin/AccumulateUrlCount.sh $startTime $endTime >> $logFile
 sh $baseDir/bin/accumulateUrlCount.sh $startTime $endTime >> $logFile
 sh $baseDir/bin/crondInf.sh ${textInputRoot}/${startTime}_${endTime}
 sh $baseDir/bin/etl.sh inf result  >> $logFile
