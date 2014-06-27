a6e0d5632c2702496c2045b1591d9a8b2b8eded6
boyang070822 <boyang070822@gmail.com>
2014-06-27 11:36:19 +0800

    fix bug in crondInf.sh

--- bin/crondInf.sh
+++ bin/crondInf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 baseDir=`dirname $0`/..
 JAR=$baseDir/target/lldaMahout-1.0-SNAPSHOT-jar-with-dependencies.jar
-MAIN=com.elex.bigdata.llda.mahout.mapreduce.crond.CrondInfDriver
+MAIN=com.elex.bigdata.llda.mahout.crond.CrondInfDriver
 rootPath=/user/hadoop/user_category/lldaMahout
 inputPath=$1
 echo "hadoop jar $JAR $MAIN --input $inputPath --doc_root ${rootPath}/docs --dict_root ${rootPath}/dictionary \
