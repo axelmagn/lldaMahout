630f1055d494b97ec3e66a414f3fa25a5f9d9d64
boyang070822 <boyang070822@gmail.com>
2014-06-30 10:11:42 +0800

    log in scripts

--- bin/dayEst.sh
+++ bin/dayEst.sh
@@ -4,9 +4,12 @@ oneDayAgo=`date +%Y%m%d -d "-1 days"`
 twoDayAgo=`date +%Y%m%d -d "-2 days"`
 day=`date +%Y%m%d `
 rootPath=/user/hadoop/user_category/lldaMahout
+logFile=/data/log/user_category/processLog/llda/dayEst.log
+now=`date`
 multiInput=${rootPath}/docs/to${twoDayAgo}:${rootPath}/docs/${oneDayAgo}/*
 mergeOutput=${rootPath}/docs/to${oneDayAgo}
-sh ${baseDir}/bin/updateDict.sh  ${rootPath}/docs/${oneDayAgo}
-sh ${baseDir}/bin/mergeLDocs.sh  ${multiInput} ${mergeOutput}
+echo ${now} >> $logFile
+sh ${baseDir}/bin/updateDict.sh  ${rootPath}/docs/${oneDayAgo}  >> $logFile
+sh ${baseDir}/bin/mergeLDocs.sh  ${multiInput} ${mergeOutput}   >> $logFile
 hadoop fs -rm -r ${rootPath}/tmpModels/*
-sh ${baseDir}/bin/estDocs.sh ${mergeOutput}
+sh ${baseDir}/bin/estDocs.sh ${mergeOutput}  >> $logFile
--- bin/hourInf.sh
+++ bin/hourInf.sh
@@ -5,9 +5,10 @@ endTime=`date +%Y%m%d%H%M%S -d "-1 hours"`
 preDay=`date +%Y%m%d -d "-1 days"`
 day=`date +%Y%m%d `
 logFile=/data/log/user_category/processLog/llda/hourInf.log
-echo "fiveMinInf $startTime" >> $logFile
 textInputRoot=url_count/all_projects
+now=`date`
 #sh $baseDir/../Url_Count/bin/AccumulateUrlCount.sh $startTime $endTime >> $logFile
+echo $now >> $logFile
 sh $baseDir/bin/accumulateUrlCount.sh $startTime $endTime >> $logFile
 sh $baseDir/bin/crondInf.sh ${textInputRoot}/${startTime}_${endTime} >> $logFile
 sh $baseDir/bin/etl.sh inf result  >> $logFile
