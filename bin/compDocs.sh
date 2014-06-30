54a74982c68326d1a854cd16e160eb415cfcd116
boyang070822 <boyang070822@gmail.com>
2014-06-30 10:18:18 +0800

    log compDocs.sh

--- bin/compDocs.sh
+++ bin/compDocs.sh
@@ -5,4 +5,5 @@ MAIN=com.elex.bigdata.llda.mahout.data.mergedocs.MergeLDocDriver
 multiInput=$1
 output=$2
 uidFilePath=$3
+echo "hadoop jar $JAR $MAIN --multi_input $multiInput --output $output --uid_path $uidFilePath"
 hadoop jar $JAR $MAIN --multi_input $multiInput --output $output --uid_path $uidFilePath
