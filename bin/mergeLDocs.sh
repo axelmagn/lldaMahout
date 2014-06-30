f687846b07fd738d346f0ed2abfc546eb4256d5b
boyang070822 <boyang070822@gmail.com>
2014-06-30 10:17:58 +0800

    log mergeLDocs.sh

--- bin/mergeLDocs.sh
+++ bin/mergeLDocs.sh
@@ -4,4 +4,5 @@ JAR=$baseDir/target/lldaMahout-1.0-SNAPSHOT-jar-with-dependencies.jar
 MAIN=com.elex.bigdata.llda.mahout.data.mergedocs.MergeLDocDriver
 multiInput=$1
 output=$2
+echo "hadoop jar $JAR $MAIN --multi_input $multiInput --output $output"
 hadoop jar $JAR $MAIN --multi_input $multiInput --output $output
