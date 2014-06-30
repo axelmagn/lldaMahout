1b5f8472fb20da512f7da78ce8160d4502f90898
boyang070822 <boyang070822@gmail.com>
2014-06-30 11:30:13 +0800

    change genDocs.sh

---
+++ bin/genDocs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+baseDir=`dirname $0`/..
+JAR=$baseDir/target/lldaMahout-1.0-SNAPSHOT-jar-with-dependencies.jar
+MAIN=com.elex.bigdata.llda.mahout.data.generatedocs.GenerateLDocDriver
+rootPath=/user/hadoop/user_category/lldaMahout
+inputPath=$1
+uidPath=$3
+outputPath=$2
+echo "hadoop jar $JAR $MAIN  --dict_root ${rootPath}/dictionary  --resource_root ${rootPath}/resources \
+      --input $inputPath --output $outputPath  "
+hadoop jar $JAR $MAIN  --dict_root ${rootPath}/dictionary  --resource_root ${rootPath}/resources \
+--input $inputPath --output $outputPath
