46078a5d312c6c3e8a90a8b0662ec8ed61a52bc5
boyang070822 <boyang070822@gmail.com>
2014-06-30 11:31:56 +0800

    change genDocs.sh

--- bin/genDocAndUids.sh
+++ bin/genDocAndUids.sh
@@ -6,7 +6,8 @@ rootPath=/user/hadoop/user_category/lldaMahout
 inputPath=$1
 uidPath=$3
 outputPath=$2
+logFile=/data/log/user_category/processLog/llda/genDocsAndUids.log
 echo "hadoop jar $JAR $MAIN  --dict_root ${rootPath}/dictionary  --resource_root ${rootPath}/resources \
-      --input $inputPath --output $outputPath --uid_path $uidPath "
+      --input $inputPath --output $outputPath --uid_path $uidPath >> $logFile 2>&1"
 hadoop jar $JAR $MAIN  --dict_root ${rootPath}/dictionary  --resource_root ${rootPath}/resources \
---input $inputPath --output $outputPath --uid_path $uidPath
+--input $inputPath --output $outputPath --uid_path $uidPath >> $logFile 2>&1
--- bin/genDocs.sh
+++ bin/genDocs.sh
@@ -4,9 +4,9 @@ JAR=$baseDir/target/lldaMahout-1.0-SNAPSHOT-jar-with-dependencies.jar
 MAIN=com.elex.bigdata.llda.mahout.data.generatedocs.GenerateLDocDriver
 rootPath=/user/hadoop/user_category/lldaMahout
 inputPath=$1
-uidPath=$3
 outputPath=$2
+logFile=/data/log/user_category/processLog/llda/genDocs.log
 echo "hadoop jar $JAR $MAIN  --dict_root ${rootPath}/dictionary  --resource_root ${rootPath}/resources \
-      --input $inputPath --output $outputPath  "
+      --input $inputPath --output $outputPath  >> $logFile 2>&1"
 hadoop jar $JAR $MAIN  --dict_root ${rootPath}/dictionary  --resource_root ${rootPath}/resources \
---input $inputPath --output $outputPath
+--input $inputPath --output $outputPath  >> $logFile 2>&1
