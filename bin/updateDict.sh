e292d4afc121d56c326948cb16b856ecbd5045f4
boyang070822 <boyang070822@gmail.com>
2014-06-30 11:21:31 +0800

    add 2>&1

--- bin/updateDict.sh
+++ bin/updateDict.sh
@@ -3,7 +3,7 @@ baseDir=`dirname $0`/..
 JAR=$baseDir/target/lldaMahout-1.0-SNAPSHOT-jar-with-dependencies.jar
 MAIN=com.elex.bigdata.llda.mahout.dictionary.UpdateDictDriver
 rootPath=/user/hadoop/user_category/lldaMahout
-ogFile=/data/log/user_category/processLog/llda/updateDict.log
+logFile=/data/log/user_category/processLog/llda/updateDict.log
 echo `date` >> $logFile
 echo "hadoop jar $JAR $MAIN --input $1 --dict_root ${rootPath}/dictionary >> $logFile 2>&1"
 hadoop jar $JAR $MAIN --input $1 --dict_root ${rootPath}/dictionary  >> $logFile 2>&1
