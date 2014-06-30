effca4cc211826f36cb95a8843a2889e326bc3db
boyang070822 <boyang070822@gmail.com>
2014-06-30 10:04:26 +0800

    i can not print out uids when gen docs

--- bin/dayEst.sh
+++ bin/dayEst.sh
@@ -4,7 +4,7 @@ oneDayAgo=`date +%Y%m%d -d "-1 days"`
 twoDayAgo=`date +%Y%m%d -d "-2 days"`
 day=`date +%Y%m%d `
 rootPath=/user/hadoop/user_category/lldaMahout
-multiInput=${rootPath}/docs/to${twoDayAgo}:${rootPath}/docs/${oneDayAgo}
+multiInput=${rootPath}/docs/to${twoDayAgo}:${rootPath}/docs/${oneDayAgo}/*
 mergeOutput=${rootPath}/docs/to${oneDayAgo}
 sh ${baseDir}/bin/updateDict.sh  ${rootPath}/docs/${oneDayAgo}
 sh ${baseDir}/bin/mergeLDocs.sh  ${multiInput} ${mergeOutput}
--- bin/hourInf.sh
+++ bin/hourInf.sh
@@ -4,10 +4,10 @@ startTime=`date +%Y%m%d%H%M%S -d "-2 hours"`
 endTime=`date +%Y%m%d%H%M%S -d "-1 hours"`
 preDay=`date +%Y%m%d -d "-1 days"`
 day=`date +%Y%m%d `
-logFile=/data/log/user_category/processLog/llda/hadoopLLDA.log
+logFile=/data/log/user_category/processLog/llda/hourInf.log
 echo "fiveMinInf $startTime" >> $logFile
 textInputRoot=url_count/all_projects
 #sh $baseDir/../Url_Count/bin/AccumulateUrlCount.sh $startTime $endTime >> $logFile
 sh $baseDir/bin/accumulateUrlCount.sh $startTime $endTime >> $logFile
-sh $baseDir/bin/crondInf.sh ${textInputRoot}/${startTime}_${endTime}
+sh $baseDir/bin/crondInf.sh ${textInputRoot}/${startTime}_${endTime} >> $logFile
 sh $baseDir/bin/etl.sh inf result  >> $logFile
