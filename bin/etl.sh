efb5130fe91d1615a3c687348dcffb20a97bb785
boyang070822 <boyang070822@gmail.com>
2014-07-01 10:53:58 +0800

    etl log

--- bin/etl.sh
+++ bin/etl.sh
@@ -3,14 +3,15 @@ baseDir=`dirname $0`/..
 JAR=$baseDir/target/lldaMahout-1.0-SNAPSHOT-jar-with-dependencies.jar
 MAIN=com.elex.bigdata.llda.mahout.mapreduce.ResultEtlDriver
 rootPath=/user/hadoop/user_category/lldaMahout
+logFile=/data/log/user_category/processLog/llda/etl.log
 if [ $# -lt 3 ];then
   echo "hadoop jar $JAR $MAIN --input ${rootPath}/docTopics/$1 --output ${rootPath}/docTopics/$2 \
-  --localResultPath /data/log/user_category_result/pr "
+  --localResultPath /data/log/user_category_result/pr >> $logFile 2>&1"
   hadoop jar $JAR $MAIN --input ${rootPath}/docTopics/$1 --output ${rootPath}/docTopics/$2 \
-  --localResultPath /data/log/user_category_result/pr
+  --localResultPath /data/log/user_category_result/pr  >> $logFile 2>&1
 else
   echo "hadoop jar $JAR $MAIN --input ${rootPath}/docTopics/$1 --output ${rootPath}/docTopics/$2 \
-  --localResultPath /data/log/user_category_result/pr --result_time $3 "
+  --localResultPath /data/log/user_category_result/pr --result_time $3 >> $logFile 2>&1"
   hadoop jar $JAR $MAIN --input ${rootPath}/docTopics/$1 --output ${rootPath}/docTopics/$2 \
-  --localResultPath /data/log/user_category_result/pr --result_time $3
+  --localResultPath /data/log/user_category_result/pr --result_time $3 >> $logFile 2>&1
 fi
