8d6639bf933539d3864bc1f9f9b16580c1473cee
boyang070822 <boyang070822@gmail.com>
2014-07-02 16:40:56 +0800

    analysis

--- bin/analysis.sh
+++ bin/analysis.sh
@@ -4,5 +4,5 @@ JAR=$baseDir/target/lldaMahout-1.0-SNAPSHOT-jar-with-dependencies.jar
 MAIN=com.elex.bigdata.llda.mahout.mapreduce.analysis.AnalysisDriver
 rootPath=/user/hadoop/user_category/lldaMahout
 logFile=/data/log/user_category/processLog/llda/analysis.log
-echo "hadoop jar $JAR $MAIN --input ${rootPath}/$1 --output ${rootPath}/$2"
-hadoop jar $JAR $MAIN --input ${rootPath}/$1 --output ${rootPath}/$2
+echo "hadoop jar $JAR $MAIN --input ${rootPath}/$1 --output ${rootPath}/$2 >> $logFile 2>&1"
+hadoop jar $JAR $MAIN --input ${rootPath}/$1 --output ${rootPath}/$2 >> $logFile 2>&1
