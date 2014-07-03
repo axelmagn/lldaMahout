dd268868836d4a58acc086c21be9091e31f7694e
boyang070822 <boyang070822@gmail.com>
2014-07-03 17:18:11 +0800

    create batchFillUpInf.sh

---
+++ bin/batchFillUpInf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+baseDir=`dirname $0`/..
+day=`date +%Y%m%d `
+pattern=$1
+echo $pattern
+echo " hadoop fs -ls url_count/all_projects/ | grep ${pattern} "
+files=`hadoop fs -ls url_count/all_projects/ | grep ${pattern} | tr -s " " " " | cut -f8 -d" "`
+for file in ${files[@]};do
+  startTime=${files##*/}
+  startTime=${startTime##_*}
+  echo $startTime
+  sh $baseDir/bin/crondInf.sh ${file}
+  sh $baseDir/bin/etl.sh inf result ${startTime}
+done
