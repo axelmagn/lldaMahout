78e39e3bdfaadfad984e911f0d0c694c7ed0db0a
boyang070822 <boyang070822@gmail.com>
2014-07-03 17:52:25 +0800

    create batchFillUpInf.sh

--- bin/batchFillUpInf.sh
+++ bin/batchFillUpInf.sh
@@ -7,7 +7,7 @@ echo " hadoop fs -ls url_count/all_projects/ | grep ${pattern} "
 files=`hadoop fs -ls url_count/all_projects/ | grep ${pattern} | tr -s " " " " | cut -f8 -d" "`
 for file in ${files[@]};do
   startTime=${files##*/}
-  startTime=${startTime##_*}
+  startTime=${startTime%%_*}
   echo $startTime
   sh $baseDir/bin/crondInf.sh ${file}
   sh $baseDir/bin/etl.sh inf result ${startTime}
