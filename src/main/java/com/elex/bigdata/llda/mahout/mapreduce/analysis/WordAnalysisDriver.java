bcdb8e2a942d52253df8b89a94f94ff91cc23d60
boyang070822 <boyang070822@gmail.com>
2014-07-04 17:19:33 +0800

    change WordAnalysis

--- src/main/java/com/elex/bigdata/llda/mahout/mapreduce/analysis/WordAnalysisDriver.java
+++ src/main/java/com/elex/bigdata/llda/mahout/mapreduce/analysis/WordAnalysisDriver.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -124,57 +125,61 @@ public class WordAnalysisDriver {
   public static class WordAnalysisReducer extends Reducer<Text, IntWritable, Text, Text> {
     private int[] wordLens = new int[]{10, 30, 50, 100, 150, 200, 250, 300, 350, 400, 500, 600, 700, 800, 900, 1000};
     private int[] wordCounts = new int[wordLens.length + 1];
-    private int[] noRepeatWordCounts = new int[wordLens.length + 1];
+    Pattern pattern = Pattern.compile("(gif|GIF|jpg|JPG|png|PNG|ico|ICO|css|CSS|sit|SIT|eps|EPS|wmf|WMF|zip|ZIP|ppt|PPT|mpg|MPG|xls|XLS|gz|GZ|" +
+      "pm|RPM|tgz|TGZ|mov|MOV|exe|EXE|jpeg|JPEG|bmp|BMP|js|JS)$");
 
     private int specialUrlCount = 0, totalWordCount = 0, noRepeatTotalWordCount = 0;
 
     public void setup(Context context) {
       Arrays.fill(wordCounts, 0);
-      Arrays.fill(noRepeatWordCounts, 0);
     }
 
     public void reduce(Text key, Iterable<IntWritable> values, Context context) {
-      String[] tokens = key.toString().split(" ");
-      if (tokens.length < 2)
-        return;
-      int index = Integer.parseInt(tokens[1]);
-      if (index < 0 || index > wordCounts.length - 1) {
-        System.out.println("index is " + index + ". error!");
-        return;
-      }
-
       int count = 0;
       Iterator<IntWritable> iter = values.iterator();
       while (iter.hasNext())
         count += iter.next().get();
-      if (tokens[0].equals(REPEAT)) {
-        wordCounts[index] += count;
-        totalWordCount += count;
-      } else if (tokens[0].equals(NOREPEAT)) {
-        noRepeatWordCounts[index] += count;
-        noRepeatTotalWordCount += count;
-      } else if (tokens[0].equals(SPECIAL)) {
-        specialUrlCount += count;
+      if (!key.toString().equals(SPECIAL)) {
+        int wordLen = key.getLength();
+        int i;
+        for (i = 0; i < wordLens.length; i++) {
+          if (wordLen <= wordLens[i]) {
+            break;
+          }
+        }
+        wordCounts[i] += count;
       }
+      Matcher matcher = pattern.matcher(key.toString());
+      if (matcher.find())
+        specialUrlCount += count;
     }
 
     public void cleanup(Context context) throws IOException, InterruptedException {
       context.write(new Text("total count"), new Text(" " + totalWordCount));
 
-      context.write(new Text("repeat word count"), new Text(wordCounts.length + " groups"));
       context.write(new Text("~" + wordLens[0]), new Text(" " + wordCounts[0]));
       for (int i = 1; i < wordCounts.length - 1; i++) {
         context.write(new Text(wordLens[i - 1] + "~" + wordLens[i]), new Text(" " + wordCounts[i]));
       }
       context.write(new Text(wordLens[wordLens.length - 1] + "~"), new Text(" " + wordCounts[wordCounts.length - 1]));
-      context.write(new Text("no repeat total count"), new Text(" " + noRepeatTotalWordCount));
-      context.write(new Text("no repeat word count"), new Text(noRepeatWordCounts.length + " groups"));
-      context.write(new Text("~" + wordLens[0]), new Text(" " + noRepeatWordCounts[0]));
-      for (int i = 1; i < noRepeatWordCounts.length - 1; i++) {
-        context.write(new Text(wordLens[i - 1] + "~" + wordLens[i]), new Text(" " + noRepeatWordCounts[i]));
-      }
-      context.write(new Text(wordLens[wordLens.length - 1] + "~"), new Text(" " + noRepeatWordCounts[noRepeatWordCounts.length - 1]));
       context.write(new Text("special word "), new Text(" " + specialUrlCount));
+    }
+
+    public static class UniqWordReducer extends Reducer<Text,NullWritable,Text,NullWritable>{
+      public void reduce(Text key,Iterable<NullWritable> values,Context context) throws IOException, InterruptedException {
+        context.write(key, NullWritable.get());
+
+      }
+    }
+
+    public static class UniqWordMapper extends Mapper<Object,Text,Text,NullWritable>{
+      public void map(Object key,Text value,Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        String[] tokens = line.split("\t");
+        if (tokens.length < 3)
+          return;
+        context.write(new Text(tokens[1]), NullWritable.get());
+      }
     }
 
 
