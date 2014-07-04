81f8a433da25bc3b8d939ac22f60815b28ff3476
boyang070822 <boyang070822@gmail.com>
2014-07-04 11:46:11 +0800

    change analysis

--- bin/analysis.sh
+++ bin/analysis.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 baseDir=`dirname $0`/..
 JAR=$baseDir/target/lldaMahout-1.0-SNAPSHOT-jar-with-dependencies.jar
-MAIN=com.elex.bigdata.llda.mahout.mapreduce.analysis.UserAnalysisDriver
+
 rootPath=/user/hadoop/user_category/lldaMahout
 logFile=/data/log/user_category/processLog/llda/analysis.log
-echo "hadoop jar $JAR $MAIN --input ${rootPath}/$1 --output ${rootPath}/$2 >> $logFile 2>&1"
-hadoop jar $JAR $MAIN --input ${rootPath}/$1 --output ${rootPath}/$2 >> $logFile 2>&1
+if [[ $# < 3 ]]
+then
+  echo " args < 3"
+  exit 1
+fi
+type=$1
+if [[ $type = user ]]
+  then
+     MAIN=com.elex.bigdata.llda.mahout.mapreduce.analysis.UserAnalysisDriver
+     echo "hadoop jar $JAR $MAIN --input ${rootPath}/$2 --output ${rootPath}/$3 >> $logFile 2>&1"
+     hadoop jar $JAR $MAIN --input ${rootPath}/$2 --output ${rootPath}/$3 >> $logFile 2>&1
+  else
+     MAIN=com.elex.bigdata.llda.mahout.mapreduce.analysis.WordAnalysisDriver
+     echo "hadoop jar $JAR $MAIN $2 $3 >> $logFile 2>&1"
+     hadoop jar $JAR $MAIN $2 $3 >> $logFile 2>&1
+fi
+
