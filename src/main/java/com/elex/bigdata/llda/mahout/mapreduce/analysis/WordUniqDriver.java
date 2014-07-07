c9776ff657b65771aa06ee609f2683d86dc6bad8
boyang070822 <boyang070822@gmail.com>
2014-07-07 14:56:39 +0800

    change WordUniqDriver

--- src/main/java/com/elex/bigdata/llda/mahout/mapreduce/analysis/WordUniqDriver.java
+++ src/main/java/com/elex/bigdata/llda/mahout/mapreduce/analysis/WordUniqDriver.java
@@ -52,8 +52,9 @@ public class WordUniqDriver {
 
   public static Job prepareJob(Configuration conf, Path inputPath, Path outputPath) throws IOException {
     Job job = new Job(conf);
-    conf.setLong("mapred.max.split.size", 2 * 1024 * 1024 * 1024); // 2G
     conf.setLong("mapreduce.input.fileinputformat.split.maxsize", 2 * 1000 * 1000 * 1000);
+    conf.setLong("mapreduce.input.fileinputformat.split.minsize.per.node",1*1000 * 1000 * 1000);
+    conf.setLong("mapreduce.input.fileinputformat.split.minsize.per.rack",1500*1000*1000);
     job.setMapperClass(WordUniqMapper.class);
     job.setReducerClass(WordUniqReducer.class);
     job.setInputFormatClass(CombineTextInputFormat.class);
