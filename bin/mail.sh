277f04c7179e78b045133d5e35313e7805d22cdc
boyang070822 <boyang070822@gmail.com>
2014-07-17 15:50:27 +0800

    create mail.sh to mail category result to my mail

--- bin/mail.sh
+++ bin/mail.sh
@@ -3,7 +3,7 @@ resultRoot=/data/log/user_category_result/pr
 day=`date +%Y%m%d -d "-1 days"`
 resultDir=${resultRoot}/${day}
 categorys=(a b c d z)
-for category in categorys;do
+for category in ${categorys[@]};do
    echo ${category}
    echo ${category} >> ${resultDir}/result
    cat ${resultDir}/* | grep ${category}$ | wc -l >> ${resultDir}/result
