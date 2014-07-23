f17f13dfcb37d670fc6090878583a9a0b1dedb9e
boyang070822 <boyang070822@gmail.com>
2014-07-23 17:10:32 +0800

    create WordCleanDriver

--- src/main/java/com/elex/bigdata/llda/mahout/mapreduce/analysis/WordCleanDriver.java
+++ src/main/java/com/elex/bigdata/llda/mahout/mapreduce/analysis/WordCleanDriver.java
@@ -51,12 +51,11 @@ public class WordCleanDriver extends AbstractJob {
     if(fs.exists(outputPath))
       fs.delete(outputPath,true);
     job.setMapperClass(WordCleanMapper.class);
-    job.setCombinerClass(WordCleanReducer.class);
     job.setReducerClass(WordCleanReducer.class);
     job.setMapOutputKeyClass(Text.class);
     job.setMapOutputValueClass(Text.class);
     job.setInputFormatClass(CombineTextInputFormat.class);
-    job.setNumReduceTasks(2);
+    job.setNumReduceTasks(4);
     FileInputFormat.addInputPath(job, inputPath);
     job.setOutputFormatClass(TextOutputFormat.class);
     FileOutputFormat.setOutputPath(job, outputPath);
