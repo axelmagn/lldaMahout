bc432a36d5b0f6a2ef11c6050f9fdb2ea65dbaf9
boyang070822 <boyang070822@gmail.com>
2014-07-23 18:36:45 +0800

    change accumulateUrlCount.sh

--- bin/accumulateUrlCount.sh
+++ bin/accumulateUrlCount.sh
@@ -8,4 +8,4 @@ echo `date` >> $logFile
 echo "hadoop jar $JAR  $MAIN --outputBase $outputBase --startTime $1 --endTime $2 >> $logFile 2>&1"
 hadoop jar $JAR  $MAIN --outputBase $outputBase --startTime $1 --endTime $2  >> $logFile 2>&1
 echo "sh ${baseDir}/bin/analysis wordClean ${outputBase}/$1_$2 ${outputBase}/clean/$1_$2"
-sh ${baseDir}/bin/analysis wordClean ${outputBase}/$1_$2 ${outputBase}/clean/$1_$2
+sh ${baseDir}/bin/analysis.sh wordClean ${outputBase}/$1_$2 ${outputBase}/clean/$1_$2
