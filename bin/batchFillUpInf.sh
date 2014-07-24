a8f41a4757cedc7cd81d228b160944715e44b211
boyang070822 <boyang070822@gmail.com>
2014-07-24 16:46:06 +0800

    remove log

--- bin/batchFillUpInf.sh
+++ bin/batchFillUpInf.sh
@@ -3,6 +3,8 @@ baseDir=`dirname $0`/..
 day=`date +%Y%m%d `
 pattern=$1
 echo $pattern
+resultRoot=user_category/lldaMahout/docTopics
+localResultRoot=/data/log/user_category_result/pr
 echo " hadoop fs -ls url_count/all_projects/clean | grep ${pattern} "
 files=`hadoop fs -ls url_count/all_projects/clean | grep ${pattern} | tr -s " " " " | cut -f8 -d" "`
 echo ${files[@]}
@@ -11,5 +13,5 @@ for file in ${files[@]};do
   startTime=${startTime%%_*}
   echo $startTime
   sh $baseDir/bin/crondInf.sh ${file}
-  sh $baseDir/bin/etl.sh inf result ${startTime}
+  sh $baseDir/bin/etl.sh ${resultRoot}/inf ${resultRoot}/inf_result ${localResultRoot} $startTime >> $logFile
 done
