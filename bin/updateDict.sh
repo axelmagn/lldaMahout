56b97062f72af0f46b989cad78f0cb5f62ec42d5
boyang070822 <boyang070822@gmail.com>
2014-07-28 15:29:07 +0800

    update UpdateDictReducer

--- bin/updateDict.sh
+++ bin/updateDict.sh
@@ -6,9 +6,9 @@ rootPath=/user/hadoop/user_category/lldaMahout
 logFile=/data/log/user_category/processLog/llda/updateDict.log
 echo `date` >> $logFile
 if [ $# -ge 2 ];then
-  echo "hadoop jar $JAR $MAIN --input $1 --count_threshold $2 --dict_root ${rootPath}/dictionary >> $logFile 2>&1"
-  hadoop jar $JAR $MAIN --input $1 --count_threshold $2 --dict_root ${rootPath}/dictionary  >> $logFile 2>&1
+  echo "hadoop jar $JAR $MAIN --input $1 --count_threshold $2 --dict_root ${rootPath}/dictionary --resource_root ${rootPath}/resources >> $logFile 2>&1"
+  hadoop jar $JAR $MAIN --input $1 --count_threshold $2 --dict_root ${rootPath}/dictionary --resource_root ${rootPath}/resources  >> $logFile 2>&1
 else
-  echo "hadoop jar $JAR $MAIN --input $1  --dict_root ${rootPath}/dictionary >> $logFile 2>&1"
-  hadoop jar $JAR $MAIN --input $1  --dict_root ${rootPath}/dictionary  >> $logFile 2>&1
+  echo "hadoop jar $JAR $MAIN --input $1  --dict_root ${rootPath}/dictionary --resource_root ${rootPath}/resources >> $logFile 2>&1"
+  hadoop jar $JAR $MAIN --input $1  --dict_root ${rootPath}/dictionary --resource_root ${rootPath}/resources  >> $logFile 2>&1
 fi
