975973df09dbca67de2961c6035407d743d5c356
boyang070822 <boyang070822@gmail.com>
2014-08-12 19:09:54 +0800

    change WordClean

--- bin/reCleanWord.sh
+++ bin/reCleanWord.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 baseDir=`dirname $0`/..
-dayCount=$1
+startDayCount=$1
+endDayCount=$2
 textInputRoot=url_count/all_projects
-for((i=1;i<${dayCount};i++))do
+for((i=${startDayCount};i<${endDayCount};i++))do
   specialDay=`date +%Y%m%d -d "-${i} days"`
   hadoop fs -rm -r ${textInputRoot}/clean/${specialDay}*
   echo "sh $baseDir/bin/analysis.sh  wordClean  ${textInputRoot}/${specialDay}* ${textInputRoot}/clean/${specialDay}"
