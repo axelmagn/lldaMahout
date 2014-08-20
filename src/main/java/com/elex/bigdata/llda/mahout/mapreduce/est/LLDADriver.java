e79bd5f078350abb7a0576d579d63ea58057a946
boyang070822 <boyang070822@gmail.com>
2014-08-20 14:55:08 +0800

    fix bug for SparseMatrix

--- src/main/java/com/elex/bigdata/llda/mahout/mapreduce/est/LLDADriver.java
+++ src/main/java/com/elex/bigdata/llda/mahout/mapreduce/est/LLDADriver.java
@@ -233,7 +233,7 @@ public class LLDADriver extends AbstractJob {
     conf.set(MAX_ITERATIONS_PER_DOC, String.valueOf(maxItersPerDoc));
     conf.set(MODEL_WEIGHT, "1"); // TODO
     conf.set(TEST_SET_FRACTION, String.valueOf(testFraction));
-    conf.set("mapred.map.child.java.opts","-Xss1682k");
+    conf.set("mapred.map.child.java.opts","-Xss3036k -Xmx3036m");
     conf.set("mapred.reduce.child.java.opts","-Xmx8192m");
 
     List<Double> perplexities = Lists.newArrayList();
