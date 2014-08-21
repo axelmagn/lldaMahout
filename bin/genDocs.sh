55a9834d1390c7c399e5fbae4a71d7f06be22d74
boyang070822 <boyang070822@gmail.com>
2014-08-21 15:56:21 +0800

    use SparseRowDenseColumnMatrix instead

--- bin/genDocs.sh
+++ bin/genDocs.sh
@@ -5,7 +5,7 @@ MAIN=com.elex.bigdata.llda.mahout.data.generatedocs.GenerateLDocDriver
 rootPath=/user/hadoop/user_category/lldaMahout/test
 inputPath=$1
 outputPath=$2
-logFile=/data0/log/user_category/processLog/llda/genDocs.log
+logFile=/data0/log/user_category/processLog/llda/test/genDocs.log
 echo "hadoop jar $JAR $MAIN  --dict_root ${rootPath}/dictionary  --resource_root ${rootPath}/resources \
       --input $inputPath --output $outputPath  >> $logFile 2>&1"
 hadoop jar $JAR $MAIN  --dict_root ${rootPath}/dictionary  --resource_root ${rootPath}/resources \
