9450b17012148fc2aad2d2c35fed491f7437149e
boyang070822 <boyang070822@gmail.com>
2014-08-21 16:33:51 +0800

    use SparseRowDenseColumnMatrix instead

---
+++ bin/transferUid.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+baseDir=`dirname $0`/..
+JAR=$baseDir/target/lldaMahout-1.0-SNAPSHOT-jar-with-dependencies.jar
+MAIN=com.elex.bigdata.llda.mahout.mapreduce.data.transferdocs.TransferUidDriver
+logFile=/data0/log/user_category/processLog/llda/test/transferUid.log
+inputPath=$1
+outputPath=$2
+echo "hadoop jar $JAR $MAIN --input $1 --output $2  >> $logFile 2>&1 "
+hadoop jar $JAR $MAIN --input $1 --output $2  >> $logFile 2>&1
