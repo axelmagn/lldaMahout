2721651fca3f30c2f4c250aa63d14228c7a24c55
boyang070822 <boyang070822@gmail.com>
2014-08-22 15:37:56 +0800

    log

--- bin/estDocs.sh
+++ bin/estDocs.sh
@@ -8,7 +8,7 @@ logFile=/data0/log/user_category/processLog/llda/test/est.log
 now=`date`
 echo $now >> $logFile
 echo "hadoop jar $JAR $MAIN  --input $inputDocs -k 42 --output ${rootPath}/models --maxIter 40 -mipd 1 --dictionary ${rootPath}/dictionary/dict --resource_root ${rootPath}/resources \
-      -dt ${rootPath}/docTopics/est -mt ${rootPath}/tmpModels --num_reduce_tasks 1 --num_train_threads 8 --num_update_threads 4 --test_set_fraction 0.1 --iteration_block_size 5 >> $logFile 2>&1"
-hadoop jar $JAR $MAIN  --input $inputDocs -k 42 --output ${rootPath}/test/models --maxIter 40 -mipd 1 --dictionary ${rootPath}/dictionary/dict --resource_root ${rootPath}/resources \
--dt ${rootPath}/docTopics/est -mt ${rootPath}/tmpModels --num_reduce_tasks 1 --num_train_threads 8 --num_update_threads 4 --test_set_fraction 0.1 --iteration_block_size 5 >> $logFile 2>&1
+      -dt ${rootPath}/docTopics/est -mt ${rootPath}/tmpModels --num_reduce_tasks 5 --num_train_threads 8 --num_update_threads 4 --test_set_fraction 0.1 --iteration_block_size 3 >> $logFile 2>&1"
+hadoop jar $JAR $MAIN  --input $inputDocs -k 42 --output ${rootPath}/models --maxIter 40 -mipd 1 --dictionary ${rootPath}/dictionary/dict --resource_root ${rootPath}/resources \
+-dt ${rootPath}/docTopics/est -mt ${rootPath}/tmpModels --num_reduce_tasks 5 --num_train_threads 8 --num_update_threads 4 --test_set_fraction 0.1 --iteration_block_size 3 >> $logFile 2>&1
 
