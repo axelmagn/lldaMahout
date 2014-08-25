5805c69ae9e2ae1349d7f7714d9a2727b9c49559
boyang070822 <boyang070822@gmail.com>
2014-08-25 09:41:49 +0800

    log

--- bin/specialDirMail.sh
+++ bin/specialDirMail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-resultRoot=/data0/log/user_category_result/pr/test
+resultRoot=/data0/log/user_category_result/pr
 day=$1
 resultDir=${resultRoot}/${day}
 resultFile=${resultDir}/result
