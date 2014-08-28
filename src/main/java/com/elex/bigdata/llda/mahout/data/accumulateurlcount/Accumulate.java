545c6143f799e7298abccbeddd3fe4b97daa24bc
boyang070822 <boyang070822@gmail.com>
2014-08-28 15:45:03 +0800

    change accumulate

--- src/main/java/com/elex/bigdata/llda/mahout/data/accumulateurlcount/Accumulate.java
+++ src/main/java/com/elex/bigdata/llda/mahout/data/accumulateurlcount/Accumulate.java
@@ -2,22 +2,16 @@ package com.elex.bigdata.llda.mahout.data.accumulateurlcount;
 
 import com.elex.bigdata.llda.mahout.data.accumulateurlcount.tables.*;
 import com.elex.bigdata.llda.mahout.util.FileSystemUtil;
-import com.elex.bigdata.util.MetricMapping;
-import com.xingcloud.xa.hbase.filter.SkipScanFilter;
-import com.xingcloud.xa.hbase.model.KeyRange;
-import com.xingcloud.xa.hbase.model.KeyRangeComparator;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.client.*;
-import org.apache.hadoop.hbase.filter.Filter;
 import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
-import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.jobcontrol.ControlledJob;
+import org.apache.hadoop.mapreduce.lib.jobcontrol.JobControl;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.ToolRunner;
@@ -28,10 +22,6 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created with IntelliJ IDEA.
@@ -49,25 +39,31 @@ public class Accumulate extends AbstractJob {
   private static String OUTPUT_BASE = "outputBase";
   private static String STARTTIME = "startTime";
   private static String ENDTIME = "endTime";
-  public static String TABLE_TYPE="table_type";
+  public static String TABLE_TYPE= "table_type";
 
-  private static final String[] CustomTables = new String[]{"dmp_user_action", "yac_user_action"};
-  private static final String TABLE_NAV="nav_all",TABLE_AD="ad_all_log",TABLE_GM337="gm_user_action";
+  private Map<String,SuperTable> table2Type=new HashMap<String, SuperTable>();
   public Accumulate() {
 
   }
 
-  public Accumulate(String outputBase, String startTime, String endTime) throws ParseException, IOException {
+  public Accumulate(String outputBase, String startTime, String endTime) throws ParseException, IOException, IllegalAccessException, InstantiationException, ClassNotFoundException {
     init(outputBase, startTime, endTime);
   }
 
-  private void init(String outputBase, String startTime, String endTime) throws ParseException, IOException {
+  private void init(String outputBase, String startTime, String endTime) throws ParseException, IOException, ClassNotFoundException, IllegalAccessException, InstantiationException {
     this.outputBase = outputBase;
     output=outputBase+File.separator+startTime+"_"+endTime;
     SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
     startTimeStamp = dateFormat.parse(startTime).getTime();
     endTimeStamp = dateFormat.parse(endTime).getTime();
     conf = HBaseConfiguration.create();
+    Properties properties=new Properties();
+    properties.load(this.getClass().getResourceAsStream("/table_type"));
+    for(String key: properties.stringPropertyNames()){
+       SuperTable superTable=(SuperTable)Class.forName(properties.getProperty(key)).newInstance();
+       table2Type.put(key,superTable);
+       System.out.println("table: "+key+" type:"+superTable.getClass().getName());
+    }
   }
 
   public static void main(String[] args) throws Exception {
@@ -83,29 +79,47 @@ public class Accumulate extends AbstractJob {
     if (parseArguments(args) == null)
       return -1;
     init(getOption(OUTPUT_BASE), getOption(STARTTIME), getOption(ENDTIME));
-    runJob(TABLE_AD,new AdTable(),startTimeStamp,endTimeStamp);
-    runJob(TABLE_NAV,new NavTable(),startTimeStamp,endTimeStamp);
-    runJob(TABLE_GM337,new Gm337Table(),startTimeStamp,endTimeStamp);
-    for(String tableName: CustomTables){
-      runJob(tableName,new CustomTable(),startTimeStamp,endTimeStamp);
+    JobControl jobControl=new JobControl("accumulate "+startTimeStamp+" "+endTimeStamp);
+    ControlledJob preJob=null;
+    for(Map.Entry<String,SuperTable> entry: table2Type.entrySet()){
+      Job job=prepareJob(entry.getKey(),entry.getValue(),startTimeStamp,endTimeStamp);
+      ControlledJob currentJob=new ControlledJob(conf);
+      currentJob.setJob(job);
+      if(preJob!=null)
+        currentJob.addDependingJob(preJob);
+      preJob=currentJob;
+      jobControl.addJob(preJob);
     }
-    return 0;
+    jobControl.run();
+    Thread jcThread=new Thread(jobControl);
+    jcThread.start();
+    while(true){
+      if(jobControl.allFinished()){
+        System.out.println(jobControl.getSuccessfulJobList());
+        jobControl.stop();
+        return 0;
+      }
+      if(jobControl.getFailedJobList().size()>0){
+        System.out.println(jobControl.getFailedJobList());
+        jobControl.stop();
+        return 1;
+      }
+    }
   }
 
-  public void runJob(String tableName,SuperTable tabletype,long startTime,long endTime) throws IOException, ClassNotFoundException, InterruptedException {
+  public Job prepareJob(String tableName,SuperTable tabletype,long startTime,long endTime) throws IOException, ClassNotFoundException, InterruptedException {
     Path outputPath=new Path(output,tableName);
     FileSystemUtil.deleteOutputPath(conf,outputPath);
     conf.set(TABLE_TYPE,tabletype.getClass().getName());
     Job job=new Job(conf,"accumulate "+tableName+" "+outputPath.getName());
     Scan scan=tabletype.getScan(startTime,endTime);
-    TableMapReduceUtil.initTableMapperJob(tableName,scan,AccumulateMapper.class,Text.class, IntWritable.class,job);
+    TableMapReduceUtil.initTableMapperJob(tableName, scan, AccumulateMapper.class, Text.class, IntWritable.class, job);
     job.setReducerClass(AccumulateReducer.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(Text.class);
     job.setOutputFormatClass(TextOutputFormat.class);
     FileOutputFormat.setOutputPath(job, outputPath);
     job.setJarByClass(Accumulate.class);
-    job.submit();
-    job.waitForCompletion(true);
+    return job;
   }
 }
