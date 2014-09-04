740f655657b660a5027b37fb6bf1d2ddc108f495
boyang070822 <boyang070822@gmail.com>
2014-09-04 15:32:31 +0800

    big change

--- bin/accumulateFuncs.sh
+++ bin/accumulateFuncs.sh
@@ -45,7 +45,7 @@ function batchGetNt(){
 
 function mergeNations()
 {
-  MAIN=com.elex.bigdata.llda.mahout.data.UniqMergeDriver
+  MAIN=com.elex.bigdata.llda.mahout.data.accumulate.UniqMergeDriver
   logFile=/data0/log/user_category/processLog/llda/mergeNations.log
   hadoop jar $JAR --multi_input $1 --output $2 >> $logFile 2>&1
   echo "hadoop jar $JAR $MAIN --multi_input $1 --output $2 >> $logFile 2>&1 "
