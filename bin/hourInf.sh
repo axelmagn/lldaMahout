7bc0b48065d24881bcaa75fc1344b2bc4b0015f6
boyang070822 <boyang070822@gmail.com>
2014-09-04 18:22:57 +0800

    big change

--- bin/hourInf.sh
+++ bin/hourInf.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 baseDir=`dirname $0`/..
+JAR=$baseDir/target/lldaMahout-1.0-SNAPSHOT-jar-with-dependencies.jar
 startTime=`date +%Y%m%d%H%M%S -d "-1 hours -20 mins"`
 endTime=`date +%Y%m%d%H%M%S -d "-20 mins"`
 source $baseDir/bin/infEstFuncs.sh
