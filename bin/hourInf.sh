ff0db8ebf694491e45966b19479d99a7e204826d
boyang070822 <boyang070822@gmail.com>
2014-09-04 18:21:46 +0800

    big change

--- bin/hourInf.sh
+++ bin/hourInf.sh
@@ -2,6 +2,6 @@
 baseDir=`dirname $0`/..
 startTime=`date +%Y%m%d%H%M%S -d "-1 hours -20 mins"`
 endTime=`date +%Y%m%d%H%M%S -d "-20 mins"`
-sh $baseDir/bin/infEstFuncs.sh
+source $baseDir/bin/infEstFuncs.sh
 infOrigData $startTime $endTime
 
