34e64d845c164bf61dd919da9a62d6a59a95fac1
boyang070822 <boyang070822@gmail.com>
2014-09-10 10:44:31 +0800

    big change

--- bin/categoryAnalysisFuncs.sh
+++ bin/categoryAnalysisFuncs.sh
@@ -32,7 +32,7 @@ function anaEstResult(){
                   awk '{sum[\$2]+=1}END{for(key in sum){print key,sum[key]}}'  >> $tmpFile"
   cat ${estResultDir}/*  |   \
           awk '{sum[$2]+=1}END{for(key in sum){print key,sum[key]}}'  >> $tmpFile
-  cat ${tmpFile} | grep ^[0-9]{1,3} > $estResultDir
+  cat ${tmpFile} | grep ^[0-9]{1,3} > $estResultFile
   cat ${estResultFile} | mail -s " est category analytics to${day}" "yangbo@elex-tech.com"
   hadoop fs -rm ${rootPath}/analysis/category/*
   hadoop fs -copyFromLocal  ${estResultDir}/0.0  ${rootPath}/analysis/category/userCategory
