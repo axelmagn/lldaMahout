c9cb8e37f3e9b6e1714368dba9c47d0a7941ba4d
boyang070822 <boyang070822@gmail.com>
2014-09-12 12:09:05 +0800

    big change

--- src/main/java/com/elex/bigdata/llda/mahout/mapreduce/analysis/word/IdfDriver.java
+++ src/main/java/com/elex/bigdata/llda/mahout/mapreduce/analysis/word/IdfDriver.java
@@ -77,8 +77,8 @@ public class IdfDriver extends AbstractJob {
     public void setup(Context context) throws IOException {
        Configuration conf=new Configuration();
        String actionStr=conf.get(CALSSIFIED_URL);
+       System.out.println(actionStr);
        if(!ACTION.valueStrs().contains(actionStr)){
-         System.out.println(actionStr);
          System.out.println("for option classified_url,you should input filter,get or pass");
          throw new IOException("for option classified_url,you should input filter,get or pass");
        }
