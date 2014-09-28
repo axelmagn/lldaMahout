4af137346ea596af94a75a6ae23ca1c190351e79
griffinl <lq0987@126.com>
2014-09-28 09:25:58 +0800

    update count

--- bin/user_analysis.sh
+++ bin/user_analysis.sh
@@ -3,9 +3,9 @@ baseDir=`dirname $0`/..
 JAR=$baseDir/target/lldaMahout-1.0-SNAPSHOT-jar-with-dependencies.jar
 source $baseDir/bin/accumulateFuncs.sh
 if [ $# = 1 ] ; then
-    day = $1
+    day=$1
 else
-    day = `date -d "1 days ago" +%Y%m%d`
+    day=`date -d "1 days ago" +%Y%m%d`
 fi
 countNt ${day}
 
